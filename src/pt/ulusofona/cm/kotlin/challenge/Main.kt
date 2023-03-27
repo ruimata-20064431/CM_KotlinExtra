eed6ce4f8445a5f0c9ce73bca812fb1bedbda208
ruimata-20064431 <atamiur@gmail.com>
2023-03-27 22:51:53 +0100

    TDD #010/???   blind... just to keep it warm... ;)

--- src/pt/ulusofona/cm/kotlin/challenge/Main.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -6,12 +6,17 @@ import pt.ulusofona.cm.kotlin.challenge.models.*
 fun main() {
     val pessoas = mutableListOf<Pessoa>()
     // Create a first person
-    pessoas.add(Pessoa("Nome da Pessoa 1", Util.dateFormat("05-12-2006")))
+
+    var pessoa1 = Pessoa("Nome da Pessoa 1", Util.dateFormat("05-12-2006"))
+    var pessoa2 = Pessoa("Nome da Pessoa 2", Util.dateFormat("05-5-2006"))
+
+    pessoas.add(pessoa1)
+    pessoas.add(pessoa2)
 
 
     // output data
     for (pessoa in pessoas) {
-        println(pessoa)
+        println("\n\n===========================\n$pessoa")
         pessoa.comprarVeiculo(Bicicleta("AA-BB-01"))
         if (pessoa.temCarta()) println("Tem carta? : $pessoa.carta")
         pessoa.moverVeiculoPara("AA-BB-01", 1,1)
@@ -25,8 +30,21 @@ fun main() {
 
 
         pessoa.moverVeiculoPara("BB-AA-99", 12,12)
+    }
+
+    var pessoa3 = Pessoa("Nome da Pessoa 3", Util.dateFormat("27-3-2006"))
+
+    pessoa2.venderVeiculo("BB-AA-99", pessoa3)
+    pessoas.add(pessoa3)
 
 
+    println("\n\n\n==================================================================\n")
+    for (pessoa in pessoas){
+        println("Pessoa: $pessoa")
+        for (veiculo in pessoa.veiculos){
+            println(veiculo)
+        }
+        println("\n")
     }
 
     println("Current date is: ${Util.dateToString(Util.currentDate())}")
